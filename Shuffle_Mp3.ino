a58e66e78106bb2facc7b80daf64fe1c9c85ffc4
jmpatino5 <jmpatino5@gmail.com>
2014-10-08 19:26:40 -0500

    Corregido Problema de Plugins
    Se corrigió el problema de los plugins, sin embargo ahora la función
    ".playTrack()" me está devolviendo un 2, lo cual indica que no puede
    encontrar el archvo que está buscando.

--- Shuffle_Mp3.ino
+++ Shuffle_Mp3.ino
@@ -5,7 +5,7 @@
 
 SdFat sd;							// SD Card Object
 SFEMP3Shield MP3player;				// MP3 Shield Object
-
+const int chipselect = 9;
 
 #define c_min 1 					// min numero de canciones en sd
 #define c_max 9					    //max numero de canciones en sd
@@ -17,51 +17,31 @@ int track_l= 0;
 
 void setup() {						//Se setea la comunicación serial
 
-	Serial.begin(9600);
-	sd.begin(9,SPI_HALF_SPEED);
+	Serial.begin(115200);
+	sd.begin(chipselect,SPI_HALF_SPEED);
 	if(!sd.begin(SD_SEL, SPI_HALF_SPEED)) {sd.initErrorHalt();}
 	MP3player.begin();
 	MP3player.setVolume(v_min);
-	//if(MP3player.begin() != 0) {Serial.print(F("ERROR"));}
+	if(MP3player.begin() != 0) //Prueba de error!!
+	{
+
+		uint8_t error1 = MP3player.begin(); //Acá estoy guardando el número de error que me saca al tratar de inicializar
+		Serial.print(error1);				//El error que saca es el 6 que dice "6 Patch was not loaded successfully. This may result in playTrack errors"
+
+	}
 	
 }
+
 
 void loop()
 {
 	SeeknPlay();
 }
 
-String selectartist()	//Método que pregunta por el artista que se quiere escuchar
-{
-  Serial.println("\nSeleccione el artista que quiere escuchar. ");
-  Serial.println("\n1. AndOne");
-  Serial.println("\n2. Melotron");
-  Serial.println("\n3. Front242");
-  String art = "";
-  while (art == "")
-  {
-      art = readString();
-  }
-  return art;
-}
-
-
-String readString()		//Método que permite obtener, concatenar y almacenar lo que llegue por el puerto serial
-{
-  String output = "";
-  while (Serial.available())
-  {
-    output.concat(char(Serial.read()));
-    delay(10);
-  }
-  return output;
-}
-
-
 
 void SeeknPlay() 										//Método principal que permite buscar, validar y repoducir la canción
 {
-	String inp = selectartist();						//Llama el método que trae el artista seleccionado y lo guarda en una variable string
+	String inp = selectartist(); 						//Llama el método que trae el artista seleccionado y lo guarda en una variable string
 	int cont = 0;										
 	uint8_t track_n;
 	while(cont == 0)
@@ -72,13 +52,19 @@ void SeeknPlay() 										//Método principal que permite buscar, validar y rep
 	while(!encontrado)
 	{
 		randomSeed(millis());                            //inicializa el generador de números aleatorios con la funcion millis para que sea siempre diferente en cada corrida
-		track_n = (random(c_min,c_max+1));				 //Selecciona el número aleatorio entre el rango máximo de canciones
+		uint8_t track_n = (random(c_min,c_max+1));				 //Selecciona el número aleatorio entre el rango máximo de canciones
 	    while (track_n == track_l)						 //Mientras que el número de canción sea igual que el anterior selecciona otro
 	    { 
-	      	track_n = int(random(c_min, c_max + 0.99));  
+	      	uint8_t track_n = (random(c_min, c_max + 0.99));
+
 	    }
 	    MP3player.playTrack(track_n);					 //Inicia la controlducción de la canción que seleccionó para obtener el dato del artista
-		char memory[30];								 //Separa un espacio de memoria de 16 bits para el artista
+		if(MP3player.playTrack(track_n) != 0) //Prueba de error!!
+		{
+			uint8_t error1 = MP3player.playTrack(track_n); //Acá estoy guardando el número de error que me saca al tratar de inicializar
+			Serial.print(error1);				         //El error que saca es el 6 que dice "6 Patch was not loaded successfully. This may result in playTrack errors"
+		}
+		char memory[16];								 //Separa un espacio de memoria de 16 bits para el artista
 		MP3player.trackArtist(memory);					 //Almacena el artista en el espacio de memoria
 		MP3player.stopTrack();							 
 		if(String(memory) == inp)						 //Convierte lo que haya en la memoria a un string, y lo compara con el artista seleccionado
@@ -94,7 +80,7 @@ void SeeknPlay() 										//Método principal que permite buscar, validar y rep
 		Serial.print("\nTrack ");						 							
 		Serial.print(track_n);							 //Muestra el número de la pista que está controlduciendo
 		track_l = track_n;								 //Iguala las dos canciones para poder luego comparar para no repetirlas		
-		char memory[30];
+		char memory[16];
 		MP3player.trackTitle(memory);					 //Obtiene el título de la canción y lo almacena en la memoria
 		Serial.print(" - ");					
 		Serial.println(memory);
@@ -151,6 +137,33 @@ void SeeknPlay() 										//Método principal que permite buscar, validar y rep
 		}
 	}
 	}
+}
+
+String selectartist()	//Método que pregunta por el artista que se quiere escuchar
+{
+  Serial.println("\nSeleccione el artista que quiere escuchar. ");
+  Serial.println("\n1. AndOne");
+  Serial.println("\n2. Melotron");
+  Serial.println("\n3. Front242");
+  String art = "";
+  while (art == "")
+  {
+      art = readString();
+  }
+  return art;
+}
+
+
+String readString()		//Método que permite obtener, concatenar y almacenar lo que llegue por el puerto serial
+{
+  String output = "";
+  while (Serial.available())
+  {
+    output.concat(char(Serial.read()));
+    delay(10);
+  }
+  return output;
+
 }
 
 
